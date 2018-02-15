717eff8a435785d6b0b9d3a6950c03f1fdc8a731
takagotch <dyaccb@gmail.com>
2018-02-15 15:06:38 +0900

    gesture_area; touches, changeTouches, targetTouches

---
+++ touchEv.js
@@ -0,0 +1,16 @@
+//
+$("#gesture_area")
+.on("touchstart", displayTouchesInfo).on("touchmove", displayTouchesInfo)
+.on("touchend", displayTouchesInfo).on("touchcancel", displayTouchesInfo)
+.on()
+.on();
+
+function displayTouchesInfo(ev){
+	var logMsgs = [];
+	logMsgs.push();
+	logMsgs.push();
+	logMsgs.push();
+	$.each()
+}
+
+
